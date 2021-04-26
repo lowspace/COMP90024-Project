80c3e362b2b19c4086601907ce622185bc63bf62
Zihao Hu <zihao1@student.unimelb.edu.au>
2021-04-26 17:21:18 +0800

    amend stream

--- twitter_stream/streamzihao.py
+++ twitter_stream/streamzihao.py
@@ -1,11 +1,14 @@
 # author Zihao Hu
-# time 4/23/2021
+# time 4/26/2021
 import tweepy
 import couchdb
 import configparser
 import json
 import time
 from shapely.geometry import Point, Polygon
+from http.client import IncompleteRead as http_incompleteRead
+from urllib3.exceptions import IncompleteRead as urllib3_incompleteRead
+
 
 # load json file
 def json_load(url):
@@ -14,104 +17,118 @@ def json_load(url):
         return data
 
 # read melbourne shape json
-areas = json_load("melbourne.json")["features"]
+areas = json_load("TOP4city.json")["features"]
 
-# read SA4 list
-SA4_list = []
+# read area list
+area_list = []
 for i in areas:
-    SA4_list.append(i["attributes"]["SA4_NAME16"])
-
+    area_list.append(i["attributes"]["GCC_NAME16"])
 
 def area_match(longitude, latitude, area):
     # if the point is in a polygon returns True
+    within = False
     point = Point(longitude, latitude)
-    polygon = Polygon(area["geometry"]["rings"][0])
-    return point.within(polygon)
-
+    for i in area["geometry"]["rings"]:
+        polygon = Polygon(i)
+        if point.within(polygon):
+            within = True
+    return within
 
 def coor_area(longitude, latitude, areas):
     # matching the tweet's coordinate to city
-    SA4_NAME = "out"
+    area_NAME = "out"
     for area in areas:
         if area_match(longitude, latitude, area):
-            SA4_NAME = area["attributes"]["SA4_NAME16"]
+            area_NAME = area["attributes"]["GCC_NAME16"]
             break
-    return SA4_NAME
+    return area_NAME
 
-def check_add2db(tweet):
-    t_id = tweet["_id"]
-    record = None
-    if tweet["SA4_name"] != "out" and tweet["SA4_name"] != None:
-        # tweet should be in the target area
-        for rec in cdb.view('_all_docs', key=t_id):
-            record = rec
-        if record != None:
-            print("already exist")
-        else:
-            print("got a new tweet")
-            cdb.save(tweet)
-
-def stream_tweet_norm(tweetJson):
-    # convert tweet json to normalized data
+def new_user(tweetJson, area):
+    # if new user then add to database
+    user = {}
+    user["_id"] = tweetJson["user"]["id_str"]
+    user["city"] = area
     try:
-        tweet = {}
-        tweet["_id"] = tweetJson["id_str"]
-        if tweetJson.get('user'):
-            tweet["user_id"] = tweetJson["user"]["id_str"]
-        tweet["SA4_name"] = "out"
-        tweet["text"] = tweetJson["text"]
-        tweet["language"] = tweetJson["lang"]
-        tweet['hashtags'] = None
-        if tweetJson['entities']:
-            if tweetJson['entities']['hashtags']:
-                tweet['hashtags']=tweetJson['entities']['hashtags'][0]['text']
-        if tweetJson['timestamp_ms']:
-            tweet['timestamp'] = tweetJson['timestamp_ms']
-        # locate the tweet to the SA4 region
-        if tweetJson['coordinates']:
-            # coordinates to city
-            longitude = tweetJson['coordinates']['coordinates'][0]
-            latitude = tweetJson['coordinates']['coordinates'][1]
-            tweet["SA4_name"] = coor_area(longitude, latitude, areas)
-            print("coord from 1")
-#             SA4_name = coor_area(longitude, latitude, tweetJson)
-#             if SA4_name in SA4_list:
-#                 tweet["SA4_name"] = SA4_name
-        elif tweetJson["place"] and tweetJson["place"]['bounding_box']:
-            # bounding box to city
-                box_coor = tweetJson['place']['bounding_box']['coordinates'][0]
-                longitude = (box_coor[0][0] + box_coor[2][0]) / 2
-                latitude = (box_coor[0][1] + box_coor[2][1]) / 2
-                tweet["SA4_name"] = coor_area(longitude, latitude, areas)
-                print("coord from 2")
-        else:
-            print("do not get coord")
-        # later version tweet will add to database directly
-        check_add2db(tweet)
+        udb.save(user)
+        print("Got a new user in ", area)
+        return True
+    except:
+        print("error: user exist")
+        return False
 
-    except Exception as e:
-        print(e)
-        print("something wrong in stream_tweet_norm")
-        time.sleep(10)
+def new_tweet(tweetJson, area):
+    # add user's tweet to database
+    uid = tweetJson["user"]["id"]
+#     tweepy.Cursor(api.user_timeline, user_id=uid, max_id=maxid, tweet_mode="extended")
+    cursor = tweepy.Cursor(api.user_timeline, user_id=uid)
+    for tweet in cursor.items(20):
+        tweettemp={}
+        tjson = tweet._json
+        tweettemp["_id"] = tjson["id_str"]
+        tweettemp["uid"] = tjson["user"]["id_str"]
+        tweettemp["city"] = area
+        tweettemp["value"] = tjson
+        tdb.save(tweettemp)
 
+def AddUserTweet2DB(tweetJson):
+    # locate the tweet to the region
+    if tweetJson['coordinates']:
+        # coordinates to city
+        longitude = tweetJson['coordinates']['coordinates'][0]
+        latitude = tweetJson['coordinates']['coordinates'][1]
+        t_area = coor_area(longitude, latitude, areas)
+    elif tweetJson["place"] and tweetJson["place"]['bounding_box']:
+        # bounding box to city
+            box_coor = tweetJson['place']['bounding_box']['coordinates'][0]
+            longitude = (box_coor[0][0] + box_coor[2][0]) / 2
+            latitude = (box_coor[0][1] + box_coor[2][1]) / 2
+            t_area = coor_area(longitude, latitude, areas)
+    else:
+        t_area = "out"
+    if t_area != "out" and new_user(tweetJson, t_area):
+        new_tweet(tweetJson, t_area)
 
 class MyStreamListener(tweepy.StreamListener):
-
     def on_data(self, data):
-        tweetJson = json.loads(data)
-        stream_tweet_norm(tweetJson)
+        try:
+            tweetJson = json.loads(data)
+            AddUserTweet2DB(tweetJson)
+            return True
+
+        except BaseException as e:
+            print(e)
+            time.sleep(10)
+            return True
+
+        except http_incompleteRead as e:
+            print(e)
+            time.sleep(10)
+            return True
+
+        except urllib3_incompleteRead as e:
+            print(e)
+            time.sleep(10)
+            return True
 
     def on_error(self, status_code):
         print(status_code)
+        if status_code == 429:
+            time.sleep(15 * 60 + 1)
+        if status_code == 420:
+            time.sleep(10)
+        else:
+            time.sleep(10)
+        return True
 
 if __name__ == '__main__':
 
     # Generate Authentication
     config = configparser.ConfigParser()
     try:
-        config.read("tokensimon.conf")
+        config.read("tokenzihao.conf")
     except:
         print("token Configuration not found")
+
     consumer_key = config.get('Token', 'Consumer Key')
     consumer_secret = config.get('Token', 'Consumer Secret')
     access_token = config.get('Token', 'Access Token')
@@ -120,13 +137,22 @@ if __name__ == '__main__':
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
+    try:
+        api.verify_credentials()
+        print("Authentication OK")
+    except:
+        print("Error during authentication")
 
     # Create CouchDB dataset
     server = couchdb.Server("http://admin:Aa123456789@localhost:5984")
-    if 'twitterdb' in server:
-        cdb = server['twitterdb']
+    if 'tweetdb' in server:
+        tdb = server['tweetdb']
     else:
-        cdb = server.create('twitterdb')
+        tdb = server.create('tweetdb')
+    if 'userdb' in server:
+        udb = server['userdb']
+    else:
+        udb = server.create('userdb')
 
     # Loop to save the tweets that meet the requirements in CouchDB
     while True:
@@ -136,4 +162,3 @@ if __name__ == '__main__':
 
         except Exception as e:
             print(e)
-            print("something wrong")
