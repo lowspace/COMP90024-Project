97bf594c04082699362d35d4487cab81d43116ec
Zihao Yang <yangzy3@student.unimelb.edu.au>
2021-05-02 16:48:40 +1000

    Improve scalability for couchdb

--- couchdb-cluster/reconsolidate.py
+++ couchdb-cluster/reconsolidate.py
@@ -14,6 +14,7 @@ except:
 COUCHDB_USERNAME = config.get('CouchDB', 'username')
 COUCHDB_PASSWORD = config.get('CouchDB', 'password')
 COUCHDB_ENDPOINT = config.get('CouchDB', 'endpoint')
+REPEAT = int(config.get('CouchDB', 'repeat'))
 
 def reconsolidate_cluster():
     # Get all nodes
@@ -27,24 +28,46 @@ def reconsolidate_cluster():
                     node = 'couchdb@' + container['Name']
                     all_nodes.append(node)
 
-    print(f'All nodes: {all_nodes}')
+    print(f'All nodes on this machine: {all_nodes}')
 
     for node in all_nodes: 
-        repeat = 5
         count = 0
         while True:
             url = f'http://{COUCHDB_USERNAME}:{COUCHDB_PASSWORD}@{COUCHDB_ENDPOINT}/_node/_local/_nodes/{node}'
             res = requests.put(url, json={})
-            print(f'{url} {res.json()}')
-            time.sleep(1)
-            if count > repeat and res.json().get('error') is not None:
+            if res.json().get('error') == 'conflict':
+                print(f'{node} already in cluster')
+            else: 
+                print(f'{node} {res.text}')
+            time.sleep(0.5)
+            if count > REPEAT and res.json().get('error') is not None:
                 break
             count += 1
-    
-    # Get cluster nodes
+            
+    # Remove disconnected
+    res = requests.get(f'http://{COUCHDB_USERNAME}:{COUCHDB_PASSWORD}@{COUCHDB_ENDPOINT}/_membership')
+    cluster_nodes = res.json().get('cluster_nodes') + res.json().get('all_nodes') 
+    for node in cluster_nodes: 
+        res = requests.get(f'http://{COUCHDB_USERNAME}:{COUCHDB_PASSWORD}@{COUCHDB_ENDPOINT}/_node/{node}')
+        if res.json().get('error') == 'nodedown':
+            print(f'Removing {node} due to disconnection.')
+            while True:
+                res = requests.get(f'http://{COUCHDB_USERNAME}:{COUCHDB_PASSWORD}@{COUCHDB_ENDPOINT}/_active_tasks')
+                print(f'Checking active tasks: {res.text}')
+                if len(res.json()) == 0 or res.json is None: 
+                    break
+                else: 
+                    print(f'Waiting for tasks to complete: {res.json()}')
+                    time.sleep(5)
+            res = requests.get(f'http://{COUCHDB_USERNAME}:{COUCHDB_PASSWORD}@{COUCHDB_ENDPOINT}/_node/_local/_nodes/{node}')
+            if res.json().get('_rev') is not None: 
+                rev = res.json().get('_rev')
+                res = requests.delete(f'http://{COUCHDB_USERNAME}:{COUCHDB_PASSWORD}@{COUCHDB_ENDPOINT}/_node/_local/_nodes/{node}?rev={rev}')
+                print(f'Result: {res.text}')
+
     res = requests.get(f'http://{COUCHDB_USERNAME}:{COUCHDB_PASSWORD}@{COUCHDB_ENDPOINT}/_membership')
     cluster_nodes = res.json().get('cluster_nodes')
-    print(f'Cluster nodes: {cluster_nodes}')
+    print(f'Current nodes: {cluster_nodes}')
 
 
 if __name__ == '__main__':
