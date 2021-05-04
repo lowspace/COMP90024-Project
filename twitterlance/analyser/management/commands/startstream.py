a9e0770af6032f90a1827aeb1696b44e0da5007a
Zihao Hu <huhuzizihaohao@hotmail.com>
2021-05-05 00:59:52 +0800

    startstream

--- twitterlance/analyser/management/commands/startstream.py
+++ twitterlance/analyser/management/commands/startstream.py
@@ -16,21 +16,6 @@ class Command(BaseCommand):
         except:
             print("Error during authentication")
 
-        # consumer_key = 'wku1JIpNi4pXukXp510Hzylj2'
-        # consumer_secret = 'JZPuJKqMZu929iu8XxgTQAOw0up1LLJj6hKUjFDPE4aSNsp1KP'
-        # access_token = '1382968455989583874-9DiykvjpUlnYtq2fJAgScghh9TMBs2'
-        # access_token_secret = 'k6C6Yx8LNkNp5FtyG55d6WZ0lwePYbAYAmnkg5xOd65G6'
-        #
-        # auth = stream.tweepy.OAuthHandler(consumer_key, consumer_secret)
-        # auth.set_access_token(access_token, access_token_secret)
-        # api = stream.tweepy.API(auth)
-        #
-        # try:
-        #     api.verify_credentials()
-        #     print("Authentication OK")
-        # except:
-        #     print("Error during authentication")
-
         # connect local CouchDB dataset
         server = "http://admin:Aa123456789@localhost:5984"
         couch.base_url = server
