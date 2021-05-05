801bd354a9ff17a92eaed045fd9f1af2092b6701
Zihao Hu <huhuzizihaohao@hotmail.com>
2021-05-06 01:13:58 +0800

    frontend

--- twitterlance/couchdb/couch.py
+++ twitterlance/couchdb/couch.py
@@ -2,7 +2,8 @@ import requests, uuid, json
 from django.conf import settings
 
 # Module with functions serve a Singleton
-base_url = f'http://{settings.COUCHDB_USERNAME}:{settings.COUCHDB_PASSWORD}@{settings.COUCHDB_ENDPOINT}:5984'
+base_url = f'http://admin:Aa123456789@localhost:5984'
+# base_url = f'http://{settings.COUCHDB_USERNAME}:{settings.COUCHDB_PASSWORD}@{settings.COUCHDB_ENDPOINT}:5984'
 
 # Generate a new uuid for CouchDB document
 def new_id():
