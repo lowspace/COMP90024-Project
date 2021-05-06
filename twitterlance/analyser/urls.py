f22b8b1d6fd55329c38a505d20aac1dc8d35c0ce
Xin Li <xin5@student.unimelb.edu.au>
2021-05-06 11:05:32 +0000

    aurin django view

--- twitterlance/analyser/urls.py
+++ twitterlance/analyser/urls.py
@@ -7,6 +7,7 @@ router = routers.DefaultRouter()
 router.register(r'tweets', views.TweetViewSet, basename="Tweet")
 router.register(r'users', views.UserViewSet, basename="User")
 router.register(r'sports', views.SportViewSet, basename="Sport")
+router.register(r'aurin', views.AurinViewSet, basename="Aurin")
 
 urlpatterns = [
     path('', include(router.urls)),
