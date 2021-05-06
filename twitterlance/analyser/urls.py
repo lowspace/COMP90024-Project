11cb59ea874f0593d513aabcb9e8ebd6e73c5bc4
xin <xin5@student.unimelb.edu.au>
2021-05-07 00:08:49 +1000

    aurin, sportsTweets

--- twitterlance/analyser/urls.py
+++ twitterlance/analyser/urls.py
@@ -7,6 +7,10 @@ router = routers.DefaultRouter()
 router.register(r'tweets', views.TweetViewSet, basename="Tweet")
 router.register(r'users', views.UserViewSet, basename="User")
 router.register(r'sports', views.SportViewSet, basename="Sport")
+router.register(r'aurin', views.AurinViewSet, basename="Aurin")
+router.register(r'yearly', views.YearlySportsTweetsViewSet, basename="Yearly")
+
+
 
 urlpatterns = [
     path('', include(router.urls)),
