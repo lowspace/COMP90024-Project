5c3df9d8fdcf0f4c5e1ffa0b079bcc686843087f
Simon Yang <simonyang@claimvantage.com>
2021-05-09 23:10:52 +1000

    Enhance deployment script

--- deployment/scale.sh
+++ deployment/scale.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook scale-up-instance.yaml; sleep 30; ansible-playbook -i openstack_inventory.py scale-up-swarm.yaml;
+. ./unimelb-comp90024-2021-grp-39-openrc.sh; ansible-playbook scale-up-instance.yaml; sleep 30; ansible-playbook -i openstack_inventory.py scale-up-swarm.yaml;
