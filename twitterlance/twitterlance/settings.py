a3e701b5fceb42c1053a57b26a18946dbea1fcd8
Zihao Hu <huhuzizihaohao@hotmail.com>
2021-05-10 17:27:16 +0800

    css Django

--- twitterlance/twitterlance/settings.py
+++ twitterlance/twitterlance/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "analyser/static")
+    os.path.join(BASE_DIR, "static")
 ]
 
 # STATIC_ROOT = os.path.join(BASE_DIR,)
