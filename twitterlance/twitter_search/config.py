6971f80b23cdded3b0c62fbdfa86f4b1e5aff709
lowspace <lowspacelw@gmail.com>
2021-05-12 20:28:50 +0800

    modify search

--- twitterlance/twitter_search/config.py
+++ twitterlance/twitter_search/config.py
@@ -1,12 +1,12 @@
 Geocode  = dict(
     # the string must contain 0 space
-    Melbourne = "-37.8142385,144.9622775,40km" ,
+    Adelaide  = "-34.9998826,138.3309816,40km",
     Sydney = "-33.8559799094,151.20666584,50km",
+    Melbourne = "-37.8142385,144.9622775,40km" ,
     # Perth = "-32.0391738, 115.6813561, 40km",
     Canberra ="-35.2812958,149.124822,40km",
     # Brisbane =  "-27.3812533, 152.713015, 40km",
     # Hobart ="-42.8823389, 147.3110468, 40km",
-    Adelaide  = "-34.9998826,138.3309816,40km"
 )
 
 token = dict(
@@ -15,7 +15,7 @@ token = dict(
     consumer_secret = 'AJGztH99Xj8Ch3ueleBd95nUHS2L30tmmvRjiVEpri1qZyAlc4',
     access_token_key = '1383751844057391107-Tw0FTLNESkWFgq5OB9BkvjvlOjETot',
     access_token_secret = '847AbkKGOcEp1SeiR3wNu5xvBgDaUS0EHd9iIeMx6nv50',
-    )
+    ),
     
     token_2 = dict(
     consumer_key = 'b9UKdOCbro8FfL4bOK95Argnb',
