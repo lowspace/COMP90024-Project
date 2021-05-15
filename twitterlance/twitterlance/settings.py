86aae0683f469a3c84da2f30e50c47adde23424d
lowspace <lowspacelw@gmail.com>
2021-05-15 20:01:27 +0800

    adjust to google

--- twitterlance/twitterlance/settings.py
+++ twitterlance/twitterlance/settings.py
@@ -39,7 +39,10 @@ COUCHDB_USERNAME = config.get('CouchDB', 'username')
 COUCHDB_PASSWORD = config.get('CouchDB', 'password')
 COUCHDB_ENDPOINT = config.get('CouchDB', 'endpoint')
 
-DJANGO_NODENAME =  os.environ['DJANGO_NODENAME'] if os.environ['DJANGO_NODENAME'] is not None else 'localhost'
+try:
+    DJANGO_NODENAME =  os.environ['DJANGO_NODENAME'] if os.environ['DJANGO_NODENAME'] is not None else 'localhost'
+except KeyError as e:
+    print(f'error is {e}')
 
 APPEND_SLASH = True
 
