a16152f7d2a4876b9b775ba376edb6d1abc82b92
Wei Li <liwl1@student.unimelb.edu.au>
2021-05-17 22:48:11 +0800

    add background search & update

--- twitterlance/analyser/jobs/minutely/search.py
+++ twitterlance/analyser/jobs/minutely/search.py
@@ -6,18 +6,43 @@ from django.conf import settings
 from django.core.cache import caches
 from django_extensions.management.jobs import MinutelyJob
 from couchdb import couch
+from twitter_search import search_new
+import requests, time, random
 
 
 class Job(MinutelyJob):
     help = "Node heartbeats."
 
+    def do(self):
+        time.sleep(random.randint(1, 30))  # Delay to avoid the possibility of conflicts
+        res = couch.get(f'jobs/search/')
+        if res.status_code == 404: 
+            print("Have not found the search file in jobs database.")
+            return 
+        doc = res.json()
+        if doc['status'] != 'ready':
+            return
+        doc['status'] = 'running'
+        doc['nodes'].append(settings.DJANGO_NODENAME) # Add this instance to nodes list
+        couch.updatedoc(f'jobs/search/', doc)
+        # Run the job
+        rate_limit = doc['new_users']
+        try:
+            rate_limit = int(rate_limit)
+        except:
+            rate_limit = 1
+        search_new.run_search(rate_limit)
+        # Job done on this node
+        doc = couch.get(f'jobs/search/').json()
+        if settings.DJANGO_NODENAME in doc['nodes']:
+            doc['nodes'].remove(settings.DJANGO_NODENAME)
+        if len(doc['nodes']) == 0: 
+            doc['status'] = 'idle'
+        doc['result'] = f'Job done. Last node: {settings.DJANGO_NODENAME}.'
+        couch.updatedoc(f'jobs/search/', doc)
+
     def execute(self):
-        res = couch.get('jobs/search').json()
-        print(res)
-        if res['status'] == 'ready':
-            res['status'] = 'running'
-            couch.put('jobs/search', res)
-            run_search(res['new_users'])
-            res['status'] = 'idle'
-            res['updated_at'] = couch.now()
-            couch.put('jobs/search', res)
+        try: 
+            do()
+        except Exception as e: 
+            sys.stderr.write(str(e))
