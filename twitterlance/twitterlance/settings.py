33dea903038e3cfe1cafe9b62d55bfeb90070426
Zihao Yang <yangzy3@student.unimelb.edu.au>
2021-05-17 17:19:50 +1000

    Fix database migrate error

--- twitterlance/twitterlance/settings.py
+++ twitterlance/twitterlance/settings.py
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'twitterlance.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
